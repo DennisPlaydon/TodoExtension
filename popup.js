69b5a70fdf635b8fd97a9886312c07cc74c36258
DennisPlaydon <37030744+DennisPlaydon@users.noreply.github.com>
2018-04-10 09:26:03 +1200

    added eventpage and added a new submit button

--- popup.js
+++ popup.js
@@ -1,7 +1,14 @@
 $(function(){
-	var newDate = new Date();
-	var weekday = newDate.getDay()
-    $('#name').keyup(function(){
-        $('#greet').text('Hello ' + weekday)
-    })
+    var newDate = new Date();
+    var weekdays = new Array(7);
+	weekdays[0] =  "Sunday";
+	weekdays[1] = "Monday";
+	weekdays[2] = "Tuesday";
+	weekdays[3] = "Wednesday";
+	weekdays[4] = "Thursday";
+	weekdays[5] = "Friday";
+	weekdays[6] = "Saturday";
+
+	var weekday = weekdays[newDate.getDay()]
+    $('#greet').text(weekday + "'s List")
 });
